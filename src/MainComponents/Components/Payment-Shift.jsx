40779ce51ac74c699e473bf397b8cd17f8f94c59
Sodiq Al-ameen <sodiq@tm30.net>
2024-08-02 11:20:56 +0100

    see

--- src/MainComponents/Components/Payment-Shift.jsx
+++ src/MainComponents/Components/Payment-Shift.jsx
@@ -38,6 +38,7 @@ const PaymentShift = ({ title }) => {
     setWhitecrust(false);
     setOther(true);
     setSearcher("");
+    setStartDater("");
   };
 
   // const [endDate, setEndDate] = useState(
